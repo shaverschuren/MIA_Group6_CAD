271ec6ab7c43876a27ea11d84251950e3af0cf0f
Rick van Bergen <r.j.p.v.bergen@student.tue.nl>
2019-10-22 14:42:50 +0200

    alles voor cad

---
+++ code/cad.py
@@ -0,0 +1,76 @@
+"""
+CAD module code main code.
+"""
+
+import numpy as np
+import cad_util as util
+
+
+def sigmoid(a):
+    # Computes the sigmoid function
+    # Input:
+    # a - value for which the sigmoid function should be computed
+    # Output:
+    # s - output of the sigmoid function
+
+    #-------------------------------------------------------------------#
+    #  Implement the expression for the sigmoid function. The
+    #  function must also work for vector inputs: for example if the input
+    #  is [1 -1 2] the output should be a vector of the same size with the
+    #  sigmoid values for every element of the input vector.
+
+    if type(a)=='int':
+        s=1/(1+np.exp(-a))
+    else:
+        s=np.zeros(a.shape)
+        for i in range(len(a)):
+            s[i]=1/(1+np.exp(-a[i]))
+
+    #-------------------------------------------------------------------#
+
+    return s
+
+
+def lr_nll(X, Y, Theta):
+    # Computes the negative log-likelihood (NLL) loss for the logistic
+    # regression classifier.
+    # Input:
+    # X - the data matrix
+    # Y - targets vector
+    # Theta - parameters of the logistic regression model
+    # Ouput:
+    # L - the negative log-likelihood loss
+
+    # compute the predicted probability by the logistic regression model
+
+    p = sigmoid(X.dot(Theta))
+
+    #-------------------------------------------------------------------#
+    # Implement the expression for the NLL.
+
+    L= -np.sum(Y*np.log(p)+(1-Y)*np.log(1-p))
+
+    #-------------------------------------------------------------------#
+
+    return L
+
+
+def lr_agrad(X, Y, Theta):
+    # Gradient of the negative log-likelihood for a logistic regression
+    # classifier.
+    # Input:
+    # X - the data matrix
+    # Y - targets vector
+    # Theta - parameters of the logistic regression model
+    # Example inputs:
+    # X - training_x_ones.shape=(100, 1729)
+    # Y - training_y[idx].shape=(100, 1)
+    # Theta - Theta.shape=(1729, 1)
+    # Ouput:
+    # g - gradient of the negative log-likelihood loss
+    #
+    a = X.dot(Theta)
+    p = sigmoid(a)
+    g = np.sum((p - Y)*X, axis=0).reshape(1,-1)
+
+    return g
